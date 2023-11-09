40c854b7104dfc8622912b341b72766339e88301
Kevin Fergus <kevinfergus@Kevins-MacBook-Air.local>
2023-11-09 13:05:22 -0600

    product images and descriptions displaying

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,17 @@
-import {gql} from '../utls/gql'
+import {gql} from '../utls/gql'; 
+import {formatPrice} from '../utls/formatPrice'; 
+
+import {ShopifyProduct, ShopifyExtension} from '../types/index'
+import Image from "next/image";
+import Link from "next/link";
 
 type GraphQLResponse = {
   data: {
     products: {
-      nodes: {
-        title: string;
-      }[];
+      nodes: ShopifyProduct[];
     };
   };
-  extensions: {
-    cost: {
-      requestedQueryCost: number;
-      actualQueryCost: number;
-      throttleStatus: {
-        maximumAvailable: number;
-        currentlyAvailable: number;
-        restoreRate: number;
-      };
-    };
-  };
+  extensions: ShopifyExtension;
 };
 const getProducts = async (): Promise<GraphQLResponse> => {
   const res = await fetch(process.env.GRAPHQL_API_URL!, {
@@ -32,6 +25,22 @@ const getProducts = async (): Promise<GraphQLResponse> => {
         query ProductsQuery {
           products(first: 6) {
             nodes {
+              description
+              featuredImage {
+                altText
+                height
+                id
+                url
+                width
+              }
+              handle
+              priceRangeV2 {
+                minVariantPrice {
+                  amount
+                  currencyCode
+                }
+              }
+              tags
               title
             }
           }
@@ -56,16 +65,65 @@ const HomePage = async () => {
   
   const json = await getProducts();
 
-  return (
-    <main>
-      <h1>Shopify + Next.js 13!</h1>
+  console.log('json', json.data.products)
 
-      <ul>
-        {json.data.products.nodes.map((product) => (
-          <li key={product.title}>{product.title}</li>
-        ))}
+  return (
+    <main className="container mx-auto">
+    <div className="px-5">
+      <h2 className="font-bold text-2xl mb-3">Our Products:</h2>
+      <ul className="grid grid-cols-12 gap-4 pb-12">
+        {json.data.products.nodes.map((product) => {
+  
+          return (
+            <li
+              key={product.id}
+              className="border border-slate-200 rounded-md overflow-hidden col-span-full md:col-span-6 lg:col-span-4"
+            >
+              <div>
+                <Image
+                  src={product.featuredImage.url}
+                  alt={product.featuredImage.altText}
+                  width={product.featuredImage.width}
+                  height={product.featuredImage.height}
+                  className="h-96 w-full object-cover"
+                  placeholder="blur"
+                  blurDataURL={product.featuredImage.url}
+                />
+              </div>
+  
+              <div className="p-5">
+                {product.tags.map((tag) => (
+                  <span
+                    key={tag}
+                    className="bg-yellow-400 font-bold py-1 px-3 rounded-full text-xs"
+                  >
+                    {tag}
+                  </span>
+                ))}
+  
+                <h3 className="font-medium mt-3 text-3xl">{product.title}</h3>
+  
+                <h4>
+                  {formatPrice(product.priceRangeV2.minVariantPrice.amount)}{" "}
+                  {product.priceRangeV2.minVariantPrice.currencyCode}
+                </h4>
+  
+                <p className="mt-2 mb-4">{product.description}</p>
+  
+                <Link
+                  href={`/product/`}
+                  className="border border-blue-600 inline-block p-2 rounded-md text-blue-600 hover:bg-blue-600 hover:text-white ease-in-out duration-150"
+                >
+                  View Product
+                </Link>
+              </div>
+            </li>
+          );
+        })}
       </ul>
-    </main>
+    </div>
+  </main>
+  
   );
 };
 
